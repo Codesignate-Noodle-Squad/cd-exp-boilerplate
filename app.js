85fd120b0b6e8d8b049ec1f2e96d92bd579bdf1c
paulc <cedrick@codesignate.com>
2016-01-19 23:38:49 +0800

    added some packages for database and controllers(services)

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//  **ROUTES**
 app.use('/', routes);
 app.use('/users', users);
+//  **END OF ROUTES**
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
