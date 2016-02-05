f558741e45850041fcc23166b6e36d6fffc0d0d3
paulc <cedrick@codesignate.com>
2016-02-06 01:29:47 +0800

    Make the dbconnect return a bookshelf instance

--- dbconnect.js
+++ dbconnect.js
@@ -6,5 +6,8 @@ const config = require('./config.js');
 /** Use the db in the config.js */
 const knex = require('knex')(config.db);
 
+/** Connect knex to bookshelf **/
+const bookshelf = require('bookshelf')(knex);
+
 /** exports the knext to be used by models/collections */
-export default knex;
+export default bookshelf;
