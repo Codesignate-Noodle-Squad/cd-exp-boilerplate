842e6e684e0e01846da0d23f45df8d3d84be8767
paulc <cedrick@codesignate.com>
2016-02-15 22:50:33 +0800

    Move the business logic to a controller.

--- routes/users.js
+++ routes/users.js
@@ -1,134 +1,22 @@
 var express = require('express');
 var Users = require('../models/users/collection');
 var User = require('../models/users/model');
+var UserController = require('../controllers/UserController');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    Users.forge()
-    .fetch()
-    .then(function (collection){
-        res.json({
-            success: true,
-            data: collection.toJSON()
-        })
-    })
-    .catch(function (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    });
-});
+router.get('/', UserController.getUserList);
 
 /* GET specific user via id */
-router.get('/:id', function (req, res, next) {
-    User.forge({id: req.params.id})
-    .fetch()
-    .then(function (user) {
-        if (!user) {
-            res.status(404).json({
-                success: false,
-                data: {}
-            });
-        }
-        else {
-            res.json({
-                success: true,
-                data: user.toJSON()
-            });
-        }
-    })
-    .catch(function (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    });
-});
+router.get('/:id', UserController.getSpecificUser);
 
 /* CREATE user */
-router.post('/', function (req, res, next) {
-    User.forge({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        gender: req.body.gender
-    })
-    .save()
-    .then(function (user) {
-        res.json({
-            success: true,
-            data: {id: user.get('id')}
-        });
-    })
-    .catch(function (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    });
-});
+router.post('/', UserController.createUser);
 
 /* EDIT user via id */
-router.put('/:id', function (req, res, next) {
-    User.forge({
-        id: req.params.id
-    })
-    .fetch({require: true})
-    .then(function (user) {
-        user.save({
-            first_name: req.body.firstname || user.get('first_name'),
-            last_name: req.body.lastname || user.get('last_name'),
-            email: req.body.email || user.get('email'),
-            gender: req.body.gender || user.get('gender')
-        })
-        .then(function () {
-            res.json({
-                success: true,
-                message: 'Succeesfully update data of the user'
-            });
-        })
-        .catch(function (err) {
-            res.status(500).json({
-                success: false,
-                message: err.message
-            });
-        })
-    })
-    .catch(function (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    });
-});
+router.put('/:id', UserController.editUser);
 
 /* DELETE a user via id */
-router.delete('/:id', function (req, res, next) {
-    User.forge({id: req.params.id})
-    .fetch({require: true})
-    .then(function (user) {
-        user.destroy()
-        .then(function () {
-            res.json({
-                success: true,
-                message: 'Succeesfully deleted a user'
-            });
-        })
-        .catch(function (err) {
-            res.status(500).json({
-               success: false,
-               message: err.message 
-            })
-        })
-    })
-    .catch(function (err) {
-        res.status(500).json({
-           success: false,
-           message: err.message 
-        })
-    });
-});
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
